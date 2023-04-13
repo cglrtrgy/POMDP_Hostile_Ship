351046af0e544f94fd6d2354212433e29723cb97
Turgay Caglar <turgaycglr@gmail.com>
2023-04-13 09:56:54 -0600

    added seperate file for hunting ship

--- test.jl
+++ test.jl
@@ -1,4 +1,5 @@
 include("list_states_for_random_ship.jl")
+include("list_states_for_hunting_ship.jl")
 include("gridworld.jl")
 include("helper_functions.jl")
 
@@ -10,4 +11,12 @@ for (x,y) in zip(list_of_states_random, list_of_probas_random)
     println(x,y)
 end
 
+println("Total Probability: ", sum(list_of_probas_random))
+
+
+println("Hunting Ship transition: ")
+list_of_states_random, list_of_probas_random = list_states_for_hunting_ship(4, 4, 4, 4, "hunt", false)
+for (x,y) in zip(list_of_states_random, list_of_probas_random)
+    println(x,y)
+end
 println("Total Probability: ", sum(list_of_probas_random))
