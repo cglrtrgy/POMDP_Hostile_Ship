0d9f1f80d87c24e6122f4e9c870201afbf93ee4d
Turgay Caglar <turgaycglr@gmail.com>
2023-04-13 10:04:08 -0600

    seperated shadowing ship file

--- test.jl
+++ test.jl
@@ -1,5 +1,6 @@
-include("list_states_for_random_ship.jl")
+include("list_states_for_random_ship.jl")   
 include("list_states_for_hunting_ship.jl")
+include("list_states_for_shadowing_ship.jl")
 include("gridworld.jl")
 include("helper_functions.jl")
 
@@ -20,3 +21,11 @@ for (x,y) in zip(list_of_states_random, list_of_probas_random)
     println(x,y)
 end
 println("Total Probability: ", sum(list_of_probas_random))
+
+
+println("Shadowing Ship transition: ")
+list_of_states_shadowing, list_of_probas_shadowing = list_states_for_shadowing_ship(10, 11, 11, 11, "shadow",false, 3)
+for (x,y) in zip(list_of_states_shadowing, list_of_probas_shadowing)
+    println(x,y)
+end
+println("Total Probability: ", sum(list_of_probas_shadowing))
